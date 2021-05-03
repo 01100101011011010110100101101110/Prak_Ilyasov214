de4e8d271c22f1bad5ecaf84adf2152ec45d0e8c
01100101011011010110100101101110 <s02180642@stud.cs.msu.ru>
2021-05-03 16:06:26 +0300

    second stage added(in process)

--- server/server.cpp
+++ server/server.cpp
@@ -9,6 +9,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <sys/wait.h>
 
 using namespace std;
 
@@ -22,48 +23,177 @@ int fcount(int fd) {
 	int size = 0,len;
 	char c[1024];
 	while ((len = read(fd, &c, 1024)) > 0) {
-        size += len;
-    }
+		size += len;
+	}
 	lseek(fd, 0, 0);
 	return size;
 }
 
+int char_search(char * str, char c) {
+		int j = 0;
+		while ((str[j] != '\0') && (str[j] != c)) {
+			j++;
+		}
+		if (str[j]=='0') {
+			return 0;
+		}
+		return j;
+}
+
 void Process_request(char *request, int serv_sd, int client_sd) {
-    int cont_type;
-    int len;
-    char client_request[4096];
-    if (strncmp(request, "GET /", 5)) {
+	int cont_type;
+	int len;
+	char client_request[4096];
+	if (strncmp(request, "GET /", 5)) {
 		strcpy(client_request, "HTTP/1.0 501 NotImplemented modelserver\nServer: Model HTTP Server/0.1\nConnection: keep-alive\nAllow: GET\nDate: ");
 		time_t tm = time(NULL);
 		strcat(client_request, asctime(localtime(&tm)));
 		strcat(client_request, "\n\n");
 		len = strlen(client_request);
-		//printf("%s", client_request);	//OTLADKA!!!!!!!!!!!!!!!!!!!!!!!!
+		//printf("%s", client_request);	//otladka
 		cout << client_request << endl;
-        send(client_sd, client_request, len, 0);
+       	send(client_sd, client_request, len, 0);
 		shutdown(client_sd, 1);
 		close(client_sd);
-        return;
+        
+		return;
     }
-    int i = 5;
-    int fd;
+	int i = 5;
+	int fd;
 	while(request[i] && (request[i++] != ' ')) {
-        ;
-    }
+		;
+	}
 	request[i-1] = 0;
 	i = 5;
-	while(request[i] && (request[i++] != '.')){
-        ;
+	while(request[i] && (request[i++] != '.')) {
+        	;
     }
 	cont_type = 2;
 	if (request[i]) {
-	    if(!strncmp(request + i, "html", 4)) {
-            cont_type = 0;
-        } else if (!strncmp(request + i, "jpeg", 4)) {
-            cont_type = 1;
+		if(!strncmp(request + i, "html", 4)) {
+			cont_type = 0;
+		} else if (!strncmp(request + i, "jpeg", 4)) {
+			cont_type = 1;
         }
-    }
-    if(strlen(request) == 5) {
+    } else if (strlen(request) > 5) {	//cgi
+		cont_type = 0;
+		int pid, status = 0;		//status!!
+		if ((pid = fork()) < 0) {
+			exit(1);
+		} 
+		if (pid == 0) {
+			chdir("./cgi-bin");
+			char *argv[2];
+			argv[0] = new char[(int) strlen("cgi") + 1];
+			strcpy(argv[0], "cgi");
+			argv[1] = NULL;
+			string tmp_file = "pid" + to_string(getpid()) + ".txt";
+			int tmp = open(tmp_file.c_str(), O_TRUNC | O_CREAT | O_WRONLY, 0644);
+			cerr << argv[0] << "aaa" << endl;
+			dup2(tmp, 1);
+			close(tmp);
+			int offset = char_search(request + 5, '?');
+			(request + 5)[offset] = '\0';
+			char **envp = new char *[7];
+			envp[0] = new char[(int) strlen("SERVER_ADDR=127.0.0.1") + 1];
+			strcpy(envp[0], "SERVER_ADDR=127.0.0.1");
+			envp[1] = new char[(int) strlen("CONTENT_TYPE=text/plain") + 1];
+			strcpy(envp[1], "CONTENT_TYPE=text/plain");
+			envp[2] = new char[(int) strlen("SERVER_PROTOCOL=HTTP/1.0") + 1];
+			strcpy(envp[2],"SERVER_PROTOCOL=HTTP/1.0");
+			envp[3] = new char[(int) strlen("SCRIPT_NAME=") + 1 + strlen(request + 5)];
+			strcpy(envp[3],"SCRIPT_NAME=");
+			strcat(envp[3], request + 5);
+			envp[4]=new char[(int) strlen("SERVER_PORT=") + 5];
+			char *str_port = itoa(1561);
+			strcpy(envp[4], "SERVER_PORT=");
+			strcat(envp[4], str_port);
+			delete []str_port;
+			envp[5] = new char[(int) strlen("QUERY_STRING=") + 1 + (int) strlen(request + 6 + offset)];
+			strcpy(envp[5], "QUERY_STRING=");
+			strcat(envp[5], request + 6 + offset);
+			envp[6] = NULL;
+			cerr << argv[0] << "aaa" << endl; 
+			for (int i = 0; i <= 6; i++) {
+				cerr << envp[i] << " " << i << endl;
+			}
+			execvpe(argv[0], argv, envp);
+			cerr << "cgi error\n";
+			delete []envp;
+			exit(1);
+			// обработка ошибок запуска
+		}
+		wait(&status);
+		if (WIFEXITED(status)) {
+			if (WEXITSTATUS(status) == 0) {
+				string tmp_file = "cgi-bin/pid" + to_string(pid) + ".txt";
+				int fd = open(tmp_file.c_str(), O_RDONLY);	//ok?
+				strcpy(client_request, "HTTP/1.0 200 OK modelserver\nServer: Model HTTP Server/0.1\nAllow: GET\nConnection: keep-alive\nDate: ");
+				time_t tm = time(NULL);
+				strcat(client_request, asctime(localtime(&tm)));
+				strcat(client_request, "Content-length: ");
+				char *cl = itoa(fcount(fd));	//fd empty, need cgi res
+				strcat(client_request, cl);
+				delete []cl;
+				strcat(client_request, "\nContent-type: text/plain\n\n");
+				len = strlen(client_request);
+				send(client_sd, &client_request, len, 0);
+				while((len = read(fd, client_request, 1024)) > 0) {
+					send(client_sd, &client_request, len, 0);
+				}
+				close(fd);
+				shutdown(client_sd, SHUT_RDWR);
+				close(client_sd);
+				
+				return;
+            } else {
+				//SAME?
+				cerr << "CGI has finihed with status " << WEXITSTATUS(status) << endl;
+				int fd = open("cgi.html", O_RDONLY);
+				strcpy(client_request, "HTTP/1.0 500 Internal Server Error modelserver\nServer: Model HTTP Server/0.1\nAllow: GET\nConnection: keep-alive\nDate: ");
+				time_t tm = time(NULL);
+				strcat(client_request, asctime(localtime(&tm)));
+				strcat(client_request, "Content-length: ");
+				char *cl = itoa(fcount(fd));	//fd empty, need cgi res
+				strcat(client_request, cl);
+				delete []cl;
+				strcat(client_request, "\nContent-type: text/html\n\n");
+				len = strlen(client_request);
+				send(client_sd, &client_request, len, 0);
+				while((len = read(fd, client_request, 1024)) > 0) {
+					send(client_sd, &client_request, len, 0);
+				}
+				close(fd);
+				shutdown(client_sd, SHUT_RDWR);
+				close(client_sd);
+				
+				return;
+			}
+        } else if (WIFSIGNALED(status)) {
+			cerr << "CGI has finished with signal " << WIFSIGNALED(status) << endl;
+            //Send("src/cgi.html", "HTTP/1.1 500 MyServer", Client_fd);
+			int fd = open("cgi.html", O_RDONLY);
+			strcpy(client_request, "HTTP/1.0 500 Internal Server Error modelserver\nServer: Model HTTP Server/0.1\nAllow: GET\nConnection: keep-alive\nDate: ");
+			time_t tm = time(NULL);
+			strcat(client_request, asctime(localtime(&tm)));
+			strcat(client_request, "Content-length: ");
+			char *cl = itoa(fcount(fd));	//fd empty, need cgi res
+			strcat(client_request, cl);
+			delete []cl;
+			strcat(client_request, "\nContent-type: text/html\n\n");
+			len = strlen(client_request);
+			send(client_sd, &client_request, len, 0);
+			while((len = read(fd, client_request, 1024)) > 0) {
+				send(client_sd, &client_request, len, 0);
+			}
+			close(fd);
+			shutdown(client_sd, SHUT_RDWR);
+			close(client_sd);
+		
+			return;
+		}
+	}
+	if(strlen(request) == 5) {
 		cont_type = 0;
 		fd = open("index.html", O_RDONLY);
 	} else if ((fd = open(request + 5, O_RDONLY)) < 0) {
@@ -73,12 +203,12 @@ void Process_request(char *request, int serv_sd, int client_sd) {
 		strcat(client_request, asctime(localtime(&tm)));
 		strcat(client_request, "\n\n");
 		len = strlen(client_request);
-        printf("%s", request);
-        printf("%s", client_request);   //!!!!!!!!!!!!!!!!
+        	//printf("%s", request);
+        	//printf("%s", client_request);   //otladka
 		send(client_sd, &client_request, len, 0);
 		while((len = read(fd, client_request, 1024)) > 0) {
 			send(client_sd, &client_request, len, 0);
-        }
+        	}
 		close(fd);
 		shutdown(client_sd, 1);
 		close(client_sd);
@@ -93,28 +223,27 @@ void Process_request(char *request, int serv_sd, int client_sd) {
 	delete [] cl;
 	switch (cont_type) {
 		case 0:
-            strcat(client_request, "\nContent-type: text/html\n\n");
-            break;
+            		strcat(client_request, "\nContent-type: text/html\n\n");
+            		break;
 		case 1:
-            strcat(client_request,"\nContent-type: image/jpeg\n\n");
-            break;
+            		strcat(client_request,"\nContent-type: image/jpeg\n\n");
+            		break;
 		case 2:
-            strcat(client_request,"\nContent-type: text/plain\n\n");
-            break;
+            		strcat(client_request,"\nContent-type: text/plain\n\n");
+            		break;
 	}
 	len = strlen(client_request);
-    printf("%s", client_request);       //!!!!!!!!!!!!!!!!!
+    //printf("%s", client_request);       //otladka
 	send(client_sd, &client_request, len, 0);
 	while((len = read(fd, client_request, 1024))>0) {
 		send(client_sd, &client_request, len, 0);
-    }
-
+	}
 	close(fd);
 	shutdown(client_sd, SHUT_RDWR);
 	close(client_sd);
 }
 
-class Server {      //NOTE!! CLOSE SERV_FD B4 EXIT()
+class Server {     
 private:
     int serv_sd;
     short port;
@@ -167,7 +296,7 @@ void Server::Run() {
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        cout << "Usage: " << argv[0] << " <port number>" << endl;
+    	cout << "Usage: " << argv[0] << " <port number>" << endl;
         return 0;
     } else if (atoi(argv[1]) < 1024 || atoi(argv[1]) > 49151) {
         cout << "Please provide user ports in range 1024-49151" << endl;
